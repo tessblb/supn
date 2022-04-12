46f89e36df9e3a151c7d3e75e6cc26c9df769043
Antonio Pierro <antonio.pierro@gmail.com>
2022-04-12 10:52:49 +0200

    feat: refactor counting sort

--- projects/counting_sort.py
+++ projects/counting_sort.py
@@ -1,26 +1,19 @@
 def get_count_sum(nums, k):
-    # Initialize count array
-    countSum = [0] * (k)
+    count_sum = [0] * k
+    for num in nums:
+        count_sum[num] += 1
 
-    # Store the count of each elements in count array
-    for i in range(len(nums)):
-        countSum[nums[i]] += 1
-
-    # Store the cummulative count
     for i in range(1, k):
-        countSum[i] += countSum[i - 1]
-
-    return countSum
+        count_sum[i] += count_sum[i - 1]
+    return count_sum
 
 
 def get_counting_sort(nums, k):
     # sorts the list nums,
     # whose numbers are positive integer and less than or equal to N-1
     # via a linear time complexity algorithm
-
     count_sum = get_count_sum(nums, k)
     output = [0] * len(nums)
-
     for num in nums:
         output[count_sum[num] - 1] = num
         count_sum[num] -= 1
